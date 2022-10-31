3b8407741b5c70fdd2cd11b250bdd5d6c4ddb82f
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-31 10:21:13 +0200

    add vaate controller listing

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/VaateController.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/VaateController.java
@@ -1,5 +1,20 @@
 package Ohjelmistoprojekti1.OmppuJaRane.web;
 
-public class VaateController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-}
+
+@Controller
+public class VaateController {
+	@Autowired
+	private VaatteetRepository repository; 
+	
+    @RequestMapping(value="/vaatelist")
+    public String studentList(Model model) {	
+        model.addAttribute("vaate", repository.findAll());
+        return "vaatelist";
+    }
