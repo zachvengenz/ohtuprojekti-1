a1cd92bc0904edf73ac87ca13216ae4bcaad64d6
Jussi <Jussi@MacBook-Pro-2.local>
2022-10-31 10:07:52 +0200

    entity classit valmiit

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/Vaatteet.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/Vaatteet.java
@@ -1,5 +1,116 @@
 package Ohjelmistoprojekti1.OmppuJaRane.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+
 public class Vaatteet {
+	
+	//jpa id
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	//atribuutit
+	private String name;
+	private String type;
+	private Double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Valmistaja valmistaja;
+	
+	//parametritön konstruktori
+	public Vaatteet() {}
+
+	
+	
+	//konstruktori
+	
+	public Vaatteet(String name, String type, Double price, Valmistaja valmistaja) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.valmistaja = valmistaja;
+	}
+	
+	//getterit setterit
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+
+	public Valmistaja getValmistaja() {
+		return valmistaja;
+	}
+
+
+
+	public void setValmistaja(Valmistaja valmistaja) {
+		this.valmistaja = valmistaja;
+	}
+
+
+	//toString
+	
+	@Override
+	public String toString() {
+		return "Vaatteet [id=" + id + ", name=" + name + ", type=" + type + ", price=" + price + ", valmistaja="
+				+ valmistaja + "]";
+	}
+	
+	
+	
 
 }
