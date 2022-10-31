a1dda26b968814757735c4fe1a6268902f1229b2
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-31 11:04:43 +0200

    valmistajaRepo

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/ValmistajaRepository.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/ValmistajaRepository.java
@@ -1,5 +1,14 @@
 package Ohjelmistoprojekti1.OmppuJaRane.domain;
 
-public interface ValmistajaRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface ValmistajaRepository extends CrudRepository<Valmistaja, Long>{
+
+	List<Valmistaja> findByName(String name);
 }
+
+
--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/ValmistajaController.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/ValmistajaController.java
@@ -1,5 +1,28 @@
 package Ohjelmistoprojekti1.OmppuJaRane.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import Ohjelmistoprojekti1.OmppuJaRane.domain.VaatteetRepository;
+
 public class ValmistajaController {
+
+
+
+
+	@Controller
+	public class VaatteetController {
+			@Autowired
+			private VaatteetRepository repository; 
+			
+		    @RequestMapping(value="/studentlist")
+		    public String studentList(Model model) {	
+		        model.addAttribute("students", repository.findAll());
+		        return "studentlist";
+		    }
+
+	}
 
 }
