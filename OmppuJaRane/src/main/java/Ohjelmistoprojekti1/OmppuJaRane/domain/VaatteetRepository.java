4276dbcd6af50f370837979a98d93d587a78b418
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-31 10:26:02 +0200

    vaatteetRepository edit

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/VaatteetRepository.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/domain/VaatteetRepository.java
@@ -1,5 +1,11 @@
 package Ohjelmistoprojekti1.OmppuJaRane.domain;
 
-public interface VaatteetRepository {
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VaatteetRepository extends CrudRepository<Vaate, Long> {
+	
+	List<Vaate> findByName(String name);
 
 }
