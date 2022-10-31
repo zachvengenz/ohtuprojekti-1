794e80d1990700df8e0b24ac515bb256a80911e1
Jussi <Jussi@MacBook-Pro-2.local>
2022-10-31 11:28:35 +0200

    valmistaja testidata inserted

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/OmppuJaRaneApplication.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/OmppuJaRaneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import Ohjelmistoprojekti1.OmppuJaRane.domain.Vaate;
 import Ohjelmistoprojekti1.OmppuJaRane.domain.VaatteetRepository;
 import Ohjelmistoprojekti1.OmppuJaRane.domain.Valmistaja;
 import Ohjelmistoprojekti1.OmppuJaRane.domain.ValmistajaRepository;
@@ -34,9 +35,9 @@ public class OmppuJaRaneApplication {
 				logger.info("{}", valmistaja);
 			});
 			
-			
 		
-		}
-		}
+		
+		};
 
+}
 }
