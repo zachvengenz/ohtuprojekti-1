e3cd7cbdfc22530ab343c254bdf2c368abcc08fd
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-31 14:10:09 +0200

    fixed addvalmistaja

--- OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/ValmistajaController.java
+++ OmppuJaRane/src/main/java/Ohjelmistoprojekti1/OmppuJaRane/web/ValmistajaController.java
@@ -3,6 +3,8 @@ package Ohjelmistoprojekti1.OmppuJaRane.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,12 +31,22 @@ public class ValmistajaController {
         return "addvalmistaja";
     }     
     
-    @RequestMapping(value = "/savecat", method = RequestMethod.POST)
+    @RequestMapping(value = "/savevalmistaja", method = RequestMethod.POST)
     public String save(Valmistaja valmistaja){
         valmistajarepository.save(valmistaja);
-        return "redirect:valmistajalista";
+        return "redirect:/valmistajalista";
     }    
-	
+    @GetMapping("/deletevalmistaja/{id}")
+    public String deleteValmistaja(@PathVariable("id") Long id, Valmistaja valmistaja) {
+    	valmistajarepository.deleteById(id);
+        return "redirect:/valmistajalista";
+    }
+    @RequestMapping(value = "/editvalmistaja/{id}")
+	public String editValmistaja(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("valmistaja", valmistajarepository.findById(id));
+		return "editvalmistaja";   
+    
+} 
 }
 
 
