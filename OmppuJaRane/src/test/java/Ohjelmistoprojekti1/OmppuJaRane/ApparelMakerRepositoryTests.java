898d7ce27c338fa4e0d469e2b2f50730310572b7
Jussi <Jussi@MacBook-Pro-2.local>
2022-11-07 11:32:38 +0200

    testiluokat tehty

---
+++ OmppuJaRane/src/test/java/Ohjelmistoprojekti1/OmppuJaRane/ApparelMakerRepositoryTests.java
@@ -0,0 +1,68 @@
+package Ohjelmistoprojekti1.OmppuJaRane;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import Ohjelmistoprojekti1.OmppuJaRane.domain.Apparel;
+import Ohjelmistoprojekti1.OmppuJaRane.domain.ApparelRepository;
+import Ohjelmistoprojekti1.OmppuJaRane.domain.Maker;
+import Ohjelmistoprojekti1.OmppuJaRane.domain.MakerRepository;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+
+public class ApparelMakerRepositoryTests {
+	
+	@Autowired
+	private ApparelRepository repository;
+	
+	@Autowired
+	private MakerRepository mrepository;
+	
+	@Test
+	public void findByNameShouldReturnApparel() {
+		List<Apparel> apparels = repository.findByName("JoustavaMeno");
+		
+		assertThat(apparels).hasSize(1);
+		assertThat(apparels.get(0).getType()).isEqualTo("Haalari");
+	}
+	
+	@Test
+	public void findByNameShouldReturn() {
+		List<Maker> makers = mrepository.findByName("M&M");
+		
+		assertThat(makers).hasSize(1);
+		assertThat(makers.get(0).getName()).isEqualTo("M&M");
+	}
+	
+	@Test
+	public void createNewApparel() {
+		Apparel apparel = new Apparel("Venyvä", "Liivi", 25.50, mrepository.findByName("Leikki").get(0));
+		repository.save(apparel);
+		
+		assertThat(apparel.getId()).isNotNull();
+	}
+	
+	@Test
+	public void createNewMaker() {
+		Maker maker = new Maker("Q&Q");
+		mrepository.save(maker);
+		
+		assertThat(maker.getId()).isNotNull();
+	}
+	
+	@Test
+	public void deleteApparel() {
+		Long apparelId=(long) 01;
+		
+		repository.deleteById(apparelId);
+	}
+
+}
--- OmppuJaRane/src/test/java/Ohjelmistoprojekti1/OmppuJaRane/OmppuJaRaneApplicationTests.java
+++ OmppuJaRane/src/test/java/Ohjelmistoprojekti1/OmppuJaRane/OmppuJaRaneApplicationTests.java
@@ -1,13 +1,34 @@
 package Ohjelmistoprojekti1.OmppuJaRane;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import Ohjelmistoprojekti1.OmppuJaRane.web.ApparelController;
+import Ohjelmistoprojekti1.OmppuJaRane.web.MakerController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class OmppuJaRaneApplicationTests {
+	
+	@Autowired
+	private ApparelController apparelController;
 
 	@Test
-	void contextLoads() {
+	void contextLoadsApparel() {
+		assertThat(apparelController).isNotNull();
+	}
+	
+	@Autowired
+	private MakerController makerController;
+	
+	@Test
+	void contextLoadsMaker() {
+		assertThat(makerController).isNotNull();
 	}
 
 }
