94cbf0c931a76c05fdbd4876fea73f2d8418f6ec
jussihyde <j.99hyytiainen@gmail.com>
2022-11-17 12:58:55 +0200

    controller cross origin added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [apparels, setApparels] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/apparels")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    });
+  },[])
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Omppu Ja Rane</h1>
       </header>
+     
+
+
     </div>
   );
 }
