0299cd44ad3c020f25f027edf4e6a4956b5cb6b6
Alisa Ahokas <alisa.vik@hotmail.com>
2022-12-01 12:15:45 +0200

    maker table frontend

--- frontend/src/components/MakerTable.js
+++ frontend/src/components/MakerTable.js
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function ApparelTable() {
   const [makers, setMakers] = useState([]);
 
+  const [columnDefs] = useState([
+    { field: "name", sortable: true, filter: true }
+    
+  ]);
+
+  useEffect(() => {
+    getMakers();
+  }, []);
+
+
+  const getMakers = () => {
+    fetch("http://localhost:8080/makers")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMakers(data);
+      });
+  };
+/*
   useEffect(() => {
     fetch("http://localhost:8080/makers")
       .then((response) => response.json())
       .then((data) => {
         setMakers(data);
       });
-  }, []);
-
-  return (
-    <table>
+  }, []); */
+/*
+  <table>
       <tbody>
         <tr>
           <th>Name</th>
@@ -23,6 +45,21 @@ export default function ApparelTable() {
           </tr>
         ))}
       </tbody>
-    </table>
+    {/* </table>  } */
+
+  return (
+    <>
+      <div
+        className="ag-theme-material"
+        style={{ width: "90%", height: 600, margin: "auto" }}
+      >
+        <AgGridReact
+          rowData={makers}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={10}
+        ></AgGridReact>
+      </div>
+    </>
   );
 }
